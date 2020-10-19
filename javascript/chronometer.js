1080d284af7ea9010594f122d1c161d6314d451d
Francisco Martins <fmaraposo@gmail.com>
2020-10-19 18:18:43 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    setInterval(() => {
+      this.currentTime ++;
+    },1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    if(this.currentTime === 0) {
+      return 0;
+    } else {
+      return Math.floor(this.currentTime / 60);
+    }
   }
+
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 60;
+    }
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(number) {
+    if (number.toString().length <= 2) {
+      return `0${number}`;
+    } else {
+      return number;
+    }
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval();
   }
   resetClick() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
-  }
+    return `${this.twoDigitsNumber(this.getMinutes())}:${this.twoDigitsNumber(this.getSeconds())}`;
+  } 
 }
+
+let andre = new Chronometer;
+
+andre.splitClick();
